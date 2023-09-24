9495df377ad8ba2b14c970007f34bab4a061724a
Senails <seffele@gmail.com>
2023-09-24 18:14:03 +0300

    update Types

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,10 @@
 export type  Store = { [key: string]: any };
-export type Selector<T> = (store: T)=> any;
 
 export declare function createEasyStore<T extends Store>(initialStore: T):{
-    useSelector(selectorCallback: Selector<T>):any,
-    subscribe(selectorCallback: Selector<T>, onChangeCallback: ()=>void ):{
+    useSelector<V>(selectorCallback: (store: T)=> V):V,
+    subscribe<V>(selectorCallback: (store: T)=> V, onChangeCallback: ()=>void ):{
         unSubscribe():void,
-        getStore():any,
+        getStore():V,
     },
     updateStore(mutateCallback: (copy:T)=> void),
     getStore():T,
