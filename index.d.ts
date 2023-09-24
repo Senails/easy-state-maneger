09562ed5d5b1f41f6c8ca5412d4ab1d33dce0a6d
Senails <seffele@gmail.com>
2023-09-24 18:31:10 +0300

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,8 @@
+import { ComputedRef } from "vue";
 export type  Store = { [key: string]: any };
 
 export declare function createEasyStore<T extends Store>(initialStore: T):{
-    useSelector<V>(selectorCallback: (store: T)=> V):{
-        value: V
-    },
+    useSelector<V>(selectorCallback: (store: T)=> V): ComputedRef<V>,
     subscribe<V>(selectorCallback: (store: T)=> V, onChangeCallback: ()=>void ):{
         unSubscribe():void,
         getStore():V,
